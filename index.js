c975aae9578be481350c8dcf670b41fea71b353d
mehmedaltug <mehmed.ede@hotmail.com>
2026-07-13 10:27:57 +0300

    refactor projects

--- index.js
+++ index.js
@@ -33,86 +33,86 @@ const content_bar = document.getElementById("content-bar");
 
 const projects = [
   [
-    card_generator(
+    [
       "https://github.com/COMP-302-Software-Engineering/AI_TUTOR_COMP302-Software-Project",
       "AI Tutor (Group)",
       "AI Tutor system that has student-teacher roles that uses AI to teach students about the subjects that the teaches specifies.",
-    ),
-    card_generator(
+    ],
+    [
       null,
       "Tictactoe Bot",
       "A mini-max bot that can perfectly play tictactoe. Does implement aplha-beta pruning.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/mehmedaltug/perusal-bot",
       "Perusal Bot",
       "A bot that reads the screen, re-fortmats the read text and writes the changed version to bypass filters.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/mehmedaltug/wordle-bot",
       "Wordle Bot",
       "A bot that can find today's wordle in least steps. Has a database of words and an algorithm to eliminate reduntant words.",
-    ),
+    ],
   ],
   [
-    card_generator(
+    [
       "https://github.com/mehmedaltug/simple-cpu",
       "Simple CPU",
       "A simple 8 register (plus input), 4 operation, 4 cycle cpu that can perform addition and subtraction.",
-    ),
+    ],
   ],
   [
-    card_generator(
+    [
       "https://github.com/mehmedaltug/zed-windows-portable",
       "Zed Portable Launcher",
       "A launcher program to automatically start Zed Editor in a portable manner.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/cumulonimbush/Optistella-Rust-nbody-sim",
       "Rust N-Body Simulation",
       "An N-Body simulation that utilizes the Barnes-Hut algorithm. Implemeted using Rust+Bevy",
-    ),
+    ],
   ],
   [
-    card_generator(
+    [
       "https://github.com/mehmedaltug/tabletizer-desktop",
       "Tabletizer Desktop",
       "Desktop part of the Tabletizer that handles mouse movements, local server and device communications.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/mehmedaltug/tabletizer-mobile",
       "Tabletizer Mobile",
       "Mobile part of the Tabletizer that handles connection to servers and input translation.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/mehmedaltug/expo-baseline",
       "EXPO Baseline",
       "A pre configured EXPO Nativewind project that has all the necessary tools to start developing mobile apps.",
-    ),
+    ],
   ],
   [
-    card_generator(
+    [
       "https://github.com/mehmedaltug/Simpasm",
       "Simpasm",
       "A custom language compiler that makes it easy to write assembly. Compiles to assembly instructions.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/leykuph/Comp-201-Project",
       "SPA",
       "A gui program, algorithm and data-class trilogy that finds shortest path between any two cities. (group project)",
-    ),
+    ],
   ],
   [
-    card_generator(
+    [
       "https://github.com/mehmedaltug/TinyForge-OS",
       "TinyForge OS",
       "A simple operating system that is written in pure assembly that provides a python-like environment to the user.",
-    ),
-    card_generator(
+    ],
+    [
       "https://github.com/mehmedaltug/aem-os",
       "AEM OS",
       "A debian based Linux distro that pre-install linuxbrew and flatpak that combines stability with leading edge pacakges.",
-    ),
+    ],
   ],
 ];
 
@@ -123,7 +123,7 @@ async function select_icon(id) {
 
   let content = "";
   projects[id].forEach((card) => {
-    content += card;
+    content += card_generator(...card);
   });
   content_bar.innerHTML = content;
 
@@ -136,11 +136,11 @@ async function select_icon(id) {
   fade.className = "fade-deactive";
   if (is_mobile) {
     let len = projects[id].length;
-    let dist = 15 - ((len - (len % 2)) / 2 + (len%2 == 0 ? 0:1)) * 15;
+    let dist = 15 - ((len - (len % 2)) / 2 + (len % 2 == 0 ? 0 : 1)) * 15;
     fade.style.bottom = dist.toString() + "vh";
   } else {
     let len = projects[id].length;
-    let dist = 15 - ((len - (len % 3)) / 3 + (len%3 == 0 ? 0:1)) * 15;
+    let dist = 15 - ((len - (len % 3)) / 3 + (len % 3 == 0 ? 0 : 1)) * 15;
     fade.style.bottom = dist.toString() + "vh";
   }
 }
